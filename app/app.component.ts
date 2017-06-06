56307f70afa19cd907a8aef96ee7d81c9577a6f2
Dana Weiss and Mohamed Warsame <dana@>
2017-06-06 11:28:32 -0700

    add 2 way data binding and conditional directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from '@angular/core';
       <h1>Our One-Millionth Recipe Box</h1>
       <h3>{{month}}/{{day}}/{{year}}</h3>
       <ul>
-        <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}} - {{currentRecipe.ingredients}} - {{currentRecipe.instructions}}</li>
+        <li [class]="priorityColor(currentRecipe)" (click)="isCooked(currentRecipe)" *ngFor="let currentRecipe of recipes">{{currentRecipe.title}} - {{currentRecipe.ingredients}} - {{currentRecipe.instructions}}  <button (click)="editRecipe(currentRecipe)">Edit!</button></li>
       </ul>
+      <hr>
+      <div *ngIf="selectedRecipe">
+        <h3>{{selectedRecipe.title}}</h3>
+        <p>Recipe Complete? {{selectedRecipe.cooked}}</p>
+        <h3>Edit Recipe</h3>
+        <label>Enter Recipe Title:</label>
+        <input [(ngModel)]="selectedRecipe.title">
+        <input [(ngModel)]="selectedRecipe.ingredients">
+        <label>Enter Recipe Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   `
 })
@@ -19,11 +34,39 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   recipes: Recipe[] = [
-    new Recipe('Mac and Cheese', 'Macaroni and Cheese', 'Bowl water and cook the Macaroni and add the cheese'),
-    new Recipe('PP and J sandwich', 'Bread, Peanut butter, and Jelly', 'Spread peanut butter on one slice and jelly on the other')
+    new Recipe('Mac and Cheese', 'Macaroni and Cheese', 'Bowl water and cook the Macaroni and add the cheese', 3),
+    new Recipe('PP and J sandwich', 'Bread, Peanut butter, and Jelly', 'Spread peanut butter on one slice and jelly on the other', 2)
   ];
+  selectedRecipe = null;
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
+  isCooked(clickedRecipe: Recipe) {
+    if(clickedRecipe.cooked === true) {
+      alert("This recipe is cooked!");
+    } else {
+      alert("This recipe is not cooked. Better cook now!");
+    }
+  }
+
+  priorityColor(currentRecipe){
+    if (currentRecipe.priority === 3){
+      return "bg-danger";
+    } else if (currentRecipe.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
-class Recipe {
-  constructor(public title: string, public ingredients: string, public instructions: string) {}
+export class Recipe {
+  public cooked: boolean = false;
+  constructor(public title: string, public ingredients: string, public instructions: string, public priority: number) {}
 }
